30baaf5578cf6ee26dd76049d9b7f3bfa58c1436
Karl20240601 <chenhao1699@gmail.com>
2024-08-05 15:55:20 +0800

    配置修改

--- src/main/java/io/microsphere/spring/db/event/BatchDbDataExecuteUpdateEvent.java
+++ src/main/java/io/microsphere/spring/db/event/BatchDbDataExecuteUpdateEvent.java
@@ -6,6 +6,7 @@ import org.springframework.context.ApplicationEvent;
 import java.util.List;
 
 public class BatchDbDataExecuteUpdateEvent extends ApplicationEvent {
+    private static final long serialVersionUID = -4867312420535983951L;
     private final long timestamp = System.nanoTime();
 
     private int methodIndex;
